e9ef1617428b1af21a87065d1f724f05520f0868
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2025-10-25 16:04:16 +0900

    chore: frontend setup (#1)
    * chore: update app logo, clean .gitignore, and modify index.html for SEO and favicon
    
    * chore: install tailwindcss and axios
    
    * chore: install react-router-dom and test packages
    
    * chore: install coverage-v8
    
    * chore: update tsconfig.app.json
    
    * chore: configure vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      exclude: [
+        "vite.config.ts",
+        "eslint.config.js",
+        "src/vite-env.d.ts",
+        "src/main.tsx",
+        "src/types/**",
+      ],
+    },
+  },
+});
