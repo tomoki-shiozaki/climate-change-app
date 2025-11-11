47316501349abe6946d13f5d8fbd82186c7e0f98
shiozaki <shiozaki.tomoki2@gmail.com>
2025-11-11 15:20:40 +0900

    feat(api): add temperature api view (WIP)

--- backend/apps/api/serializers/temperature.py
+++ backend/apps/api/serializers/temperature.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 
 class YearlyTemperatureSerializer(serializers.Serializer):
     year = serializers.IntegerField()
-    upper = serializers.FloatField(
-        source="Upper bound of the annual temperature anomaly (95% confidence interval)"
-    )
-    lower = serializers.FloatField(
-        source="Lower bound of the annual temperature anomaly (95% confidence interval)"
-    )
-    global_average = serializers.FloatField(
-        source="Global average temperature anomaly relative to 1861-1890"
-    )
+    upper = serializers.FloatField()
+    lower = serializers.FloatField()
+    global_average = serializers.FloatField()
