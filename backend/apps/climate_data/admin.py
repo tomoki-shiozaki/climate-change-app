fe8d27fb19987fb5b907c04e66b85741190b44d0
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2025-11-11 12:21:17 +0900

    Feat/temperature visualization (#15)
    * feat(climate_data): add IndicatorGroup model
    
    * feat(climate_data): renew migration files and add IndicatorGroup to admin
    
    * feat(fetch_temperature_data): update fetch_temperature_command for IndicatorGroup model

--- backend/apps/climate_data/admin.py
+++ backend/apps/climate_data/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from apps.climate_data.models import ClimateData, Indicator, Region
+from apps.climate_data.models import ClimateData, Indicator, IndicatorGroup, Region
 
 # Register your models here.
 admin.site.register(ClimateData)
 admin.site.register(Indicator)
+admin.site.register(IndicatorGroup)
 admin.site.register(Region)
