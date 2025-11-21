7f104a709dd303979b17ee28c83566e72daa6940
shiozaki <shiozaki.tomoki2@gmail.com>
2025-11-21 09:21:13 +0900

    refactor(constants): remove token and refresh token keys from local storage

--- frontend/src/constants/storage.ts
+++ frontend/src/constants/storage.ts
@@ -1,3 +1,1 @@
-export const LOCALSTORAGE_TOKEN_KEY = "token" as const;
-export const LOCALSTORAGE_REFRESH_TOKEN_KEY = "refreshToken" as const;
 export const LOCALSTORAGE_USERNAME_KEY = "currentUsername" as const;
