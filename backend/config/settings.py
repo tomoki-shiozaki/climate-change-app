b37c38e0d4adc94c7c08bde1cea91cee9a875382
shiozaki <shiozaki.tomoki2@gmail.com>
2025-11-30 14:23:06 +0900

    chore: add collectstatic procedure to entrypoint.prod.sh

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -215,7 +215,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
