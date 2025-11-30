a173393b195542ba95393d9b3161d313019b9ee0
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2025-11-30 14:25:00 +0900

    chore: add collectstatic procedure to entrypoint.prod.sh (#67)

--- backend/docker/entrypoint.prod.sh
+++ backend/docker/entrypoint.prod.sh
@@ -7,6 +7,10 @@ export DJANGO_SETTINGS_MODULE=config.settings
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+# 静的ファイル収集
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:$PORT \
