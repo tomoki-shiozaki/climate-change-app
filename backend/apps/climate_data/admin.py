c4cd36af805c08da5867a85825dde1f0736cc6b1
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-01 15:09:17 +0900

    Chore/django admin (#80)
    * chore(admin): update admin

--- backend/apps/climate_data/admin.py
+++ backend/apps/climate_data/admin.py
@@ -2,8 +2,33 @@ from django.contrib import admin
 
 from apps.climate_data.models import ClimateData, Indicator, IndicatorGroup, Region
 
-# Register your models here.
-admin.site.register(ClimateData)
-admin.site.register(Indicator)
-admin.site.register(IndicatorGroup)
-admin.site.register(Region)
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name", "iso_code")
+    search_fields = ("name", "iso_code")
+
+
+class IndicatorGroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class IndicatorAdmin(admin.ModelAdmin):
+    list_display = ("name", "group", "unit", "data_source_name", "fetched_at")
+    list_filter = ("group",)
+    search_fields = ("name",)
+    readonly_fields = ("fetched_at",)
+
+
+class ClimateDataAdmin(admin.ModelAdmin):
+    list_display = ("region", "indicator", "year", "value", "fetched_at")
+    list_filter = ("indicator", "region")
+    search_fields = ("region__name", "indicator__name")
+    ordering = ("indicator", "region", "year")
+    readonly_fields = ("fetched_at",)
+
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(IndicatorGroup, IndicatorGroupAdmin)
+admin.site.register(Indicator, IndicatorAdmin)
+admin.site.register(ClimateData, ClimateDataAdmin)
