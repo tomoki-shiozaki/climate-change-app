572df6618b48d7d252e3a58b06deab84b186bac3
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-01 14:51:14 +0900

    chore(admin): update admin (WIP)

--- backend/apps/climate_data/admin.py
+++ backend/apps/climate_data/admin.py
@@ -2,8 +2,31 @@ from django.contrib import admin
 
 from apps.climate_data.models import ClimateData, Indicator, IndicatorGroup, Region
 
-# Register your models here.
-admin.site.register(ClimateData)
-admin.site.register(Indicator)
-admin.site.register(IndicatorGroup)
-admin.site.register(Region)
+
+class ClimateDataAdmin(admin.ModelAdmin):
+    list_display = ("region", "indicator", "year", "value", "fetched_at")
+    list_filter = ("indicator", "region")
+    search_fields = ("region__name", "indicator__name")
+    ordering = ("indicator", "region", "year")
+
+
+class IndicatorAdmin(admin.ModelAdmin):
+    list_display = ("name", "group", "unit", "data_source_name", "fetched_at")
+    list_filter = ("group",)
+    search_fields = ("name",)
+
+
+class IndicatorGroupAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name", "iso_code")
+    search_fields = ("name", "iso_code")
+
+
+admin.site.register(ClimateData, ClimateDataAdmin)
+admin.site.register(Indicator, IndicatorAdmin)
+admin.site.register(IndicatorGroup, IndicatorGroupAdmin)
+admin.site.register(Region, RegionAdmin)
