648e664820c28b8e11c79f439a33d53385280326
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-02 12:30:19 +0900

    refactor(co2): refactor CO2DataByYearView

--- backend/apps/api/views/co2.py
+++ backend/apps/api/views/co2.py
@@ -1,34 +1,37 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from apps.api.serializers.co2 import CO2DataByYearSerializer
 from apps.climate_data.models import ClimateData, Indicator
 
 
-class CO2DataByYearView(APIView):
+class CO2DataByYearView(GenericAPIView):
     """
     フロント用 API
-    /api/co2-data/
+    /co2-data/
     """
 
-    def get(self, request):
-        # settings.py から CO2 指標グループ名と列名を取得
+    serializer_class = CO2DataByYearSerializer
+
+    def get_queryset(self):
         co2_group_info = settings.CLIMATE_GROUPS["CO2"]
         co2_group_name = co2_group_info["name"]
         co2_column_key = co2_group_info.get("column_key", "emissions_total")
 
-        # CO2 指標を単一オブジェクトとして取得
         co2_indicator = get_object_or_404(
             Indicator,
             group__name=co2_group_name,
-            name=co2_column_key,  # ここを固定文字列から設定値に変更
+            name=co2_column_key,
         )
 
-        # 指標に紐づく ClimateData を取得
-        queryset = ClimateData.objects.filter(indicator=co2_indicator).select_related(
+        return ClimateData.objects.filter(indicator=co2_indicator).select_related(
             "region"
         )
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
 
         # 年・国コードごとにまとめる
         result = {}
@@ -39,4 +42,5 @@ class CO2DataByYearView(APIView):
                 result[year] = {}
             result[year][iso] = cd.value
 
-        return Response({"data": result})
+        serializer = self.get_serializer({"data": result})
+        return Response(serializer.data)
