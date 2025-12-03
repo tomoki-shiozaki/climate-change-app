befe623ec4169d589544283daf6cdcb616d31df3
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-03 11:53:53 +0900

    chore: update cloudbuild.yaml

--- backend/docker/entrypoint.prod.sh
+++ backend/docker/entrypoint.prod.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e  # エラーがあったら即終了
 
-# Cloud Runでは migrate や collectstatic は事前実行済み
+# 本番用設定モジュール
+export DJANGO_SETTINGS_MODULE=config.settings
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+# 静的ファイル収集
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting Gunicorn..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:$PORT \
