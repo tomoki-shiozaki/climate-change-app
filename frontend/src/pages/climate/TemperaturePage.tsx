c8700793b7ab045faa565b5d9852a3e82f9c37f5
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-17 15:43:33 +0900

    chore(TemperaturePage): change descriptions

--- frontend/src/pages/climate/TemperaturePage.tsx
+++ frontend/src/pages/climate/TemperaturePage.tsx
@@ -3,8 +3,12 @@ import { TemperatureChart } from "@/features/climate/components/TemperatureChart
 const TemperaturePage = () => {
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
-      <h1 className="text-2xl font-bold mb-2 text-gray-800">ダッシュボード</h1>
-      <p className="mb-6 text-gray-600">地域ごとの気温推移グラフです。</p>
+      <h1 className="text-2xl font-bold mb-2 text-gray-800">
+        世界・北半球・南半球の気温チャート
+      </h1>
+      <p className="mb-6 text-gray-600">
+        世界、北半球、南半球それぞれの気温変化をグラフで表示しています。
+      </p>
       <div className="bg-white p-4 rounded shadow">
         <TemperatureChart />
       </div>
