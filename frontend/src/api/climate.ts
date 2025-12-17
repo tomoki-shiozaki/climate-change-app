47964f33871ee495160a907943216f49df78df59
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-17 13:44:11 +0900

    chore(api): minor improvement

--- frontend/src/api/climate.ts
+++ frontend/src/api/climate.ts
@@ -1,5 +1,5 @@
-import { apiClient } from "../features/auth/api/apiClient";
-import type { TemperatureData } from "../types/models/climate";
+import { apiClient } from "@/features/auth/api/apiClient";
+import type { TemperatureData } from "@/types/models/climate";
 
 export async function fetchTemperatureData(): Promise<TemperatureData> {
   const res = await apiClient.get<TemperatureData>("/temperature/");
