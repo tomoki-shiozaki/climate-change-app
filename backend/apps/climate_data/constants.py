ca19abeded145fea44321b59c308f6fdd5809db7
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-20 18:39:53 +0900

    refactor(climate_data): move constants to constants.py

---
+++ backend/apps/climate_data/constants.py
@@ -0,0 +1,11 @@
+CLIMATE_GROUPS = {
+    "TEMPERATURE": {
+        "name": "Temperature",
+        "description": "Temperature anomaly data from Our World in Data",
+    },
+    "CO2": {
+        "name": "CO₂ Emissions",
+        "description": "Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in tonnes.",
+        "column_key": "emissions_total",
+    },
+}
--- backend/apps/climate_data/management/commands/fetch_co2_data.py
+++ backend/apps/climate_data/management/commands/fetch_co2_data.py
@@ -1,8 +1,8 @@
 import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
+from apps.climate_data.constants import CLIMATE_GROUPS
 from apps.climate_data.models import ClimateData, Indicator, IndicatorGroup, Region
 from apps.climate_data.utils.fetch_helpers import fetch_csv
 
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         # -----------------------------
         # 指標グループを取得または作成
         # -----------------------------
-        group_info = settings.CLIMATE_GROUPS["CO2"]
+        group_info = CLIMATE_GROUPS["CO2"]
         group, _ = IndicatorGroup.objects.get_or_create(
             name=group_info["name"],
             defaults={"description": group_info["description"]},
