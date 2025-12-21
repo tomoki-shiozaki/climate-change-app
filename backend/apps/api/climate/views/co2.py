60e78b6a56632347843abc29a3fc1c639de582f6
tomoki-shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-21 15:10:08 +0900

    Refactor/climate api (#155)
    * chore(workflow): add create-superuser-develop.yml
    
    * refactor(climate-api): update co2 view
    
    * feat(climate-api): add doctstring and help_text to CO2DataByYearSerializer
    
    * test(climate-api): add tests for CO2DataByYearSerializer, CO2DataByYearView
    
    * chore(settings): ignore old django-allauth warnings
    
    * chore(pytest.ini): ignore old django-allauth warnings
    
    * chore(openapi): update openapi.d.ts

--- backend/apps/api/climate/views/co2.py
+++ backend/apps/api/climate/views/co2.py
@@ -1,46 +1,42 @@
-from django.conf import settings
+from collections import defaultdict
+
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from apps.api.climate.serializers.co2 import CO2DataByYearSerializer
+from apps.climate_data.constants import CLIMATE_GROUPS
 from apps.climate_data.models import ClimateData, Indicator
 
 
 class CO2DataByYearView(GenericAPIView):
     """
     フロント用 API
-    /co2-data/
+    /climate/co2-data/
     """
 
     serializer_class = CO2DataByYearSerializer
 
     def get_queryset(self):
-        co2_group_info = settings.CLIMATE_GROUPS["CO2"]
-        co2_group_name = co2_group_info["name"]
-        co2_column_key = co2_group_info.get("column_key", "emissions_total")
+        co2_info = CLIMATE_GROUPS["CO2"]
+        group_name = co2_info["group"]["name"]
+        name = co2_info["indicator"]["name"]
 
-        co2_indicator = get_object_or_404(
+        indicator = get_object_or_404(
             Indicator,
-            group__name=co2_group_name,
-            name=co2_column_key,
+            group__name=group_name,
+            name=name,
         )
 
-        return ClimateData.objects.filter(indicator=co2_indicator).select_related(
-            "region"
-        )
+        return ClimateData.objects.filter(indicator=indicator).select_related("region")
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
 
         # 年・国コードごとにまとめる
-        result = {}
+        result = defaultdict(dict)
         for cd in queryset:
-            year = cd.year
-            iso = cd.region.code
-            if year not in result:
-                result[year] = {}
-            result[year][iso] = cd.value
+            result[cd.year][cd.region.code] = cd.value
 
-        serializer = self.get_serializer({"co2_data": result})
+        serializer = self.get_serializer({"co2_data": dict(result)})
         return Response(serializer.data)
