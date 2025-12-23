8f21685a65ea7ed92afb69db6726ab79885c0124
shiozaki <shiozaki.tomoki2@gmail.com>
2025-12-23 12:22:44 +0900

    feat(temperature-api): add Serializers for temperature API

--- backend/apps/api/climate/serializers/temperature.py
+++ backend/apps/api/climate/serializers/temperature.py
@@ -3,16 +3,11 @@ from rest_framework import serializers
 
 class YearlyTemperatureSerializer(serializers.Serializer):
     year = serializers.IntegerField()
-    upper = serializers.FloatField()
-    lower = serializers.FloatField()
-    global_average = serializers.FloatField()
+    upper = serializers.FloatField(required=False)
+    lower = serializers.FloatField(required=False)
+    global_average = serializers.FloatField(required=False)
 
 
 class TemperatureByRegionSerializer(serializers.Serializer):
-    """
-    地域ごとの気温データを表すSerializer。
-    例: { "World": [YearlyTemperature, ...], "Northern hemisphere": [YearlyTemperature, ...] }
-    """
-
-    # 地域名: str → 値: YearlyTemperatureSerializer(many=True)
-    data = serializers.DictField(child=YearlyTemperatureSerializer(many=True))  # type: ignore
+    # 任意の地域名をキーにして、YearlyTemperature のリスト
+    __root__ = serializers.DictField(child=YearlyTemperatureSerializer(many=True))
